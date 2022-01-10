55ba5cc313e91d74d4f78c2dad2946432b92298e
EslamKhalaf9 <eslamkhalaf191@gmail.com>
2022-01-10 12:04:55 +0200

    footer component

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -27,3 +27,12 @@ const sass = gulpSass(require("sass"));
 gulp.task("sass", async () => {
   gulp.src("src/components/**/*.scss").pipe(sass()).pipe(gulp.dest("src/css"));
 });
+
+gulp.task("sass-watch", async () => {
+  gulp.watch("src/components/**/*.scss", async () => {
+    gulp
+      .src("src/components/**/*.scss")
+      .pipe(sass())
+      .pipe(gulp.dest("src/css"));
+  });
+});
